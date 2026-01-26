ef8c2423c8b642185be755945a56b48d9d1cc3b1
Ashfaq <mohammed.ashfaq1127@gmail.com>
2026-01-26 21:28:09 +0530

    fix openstreet maps errors, and fix search function in OpenstreetMaps

--- api/v1/api.py
+++ api/v1/api.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from api.v1.endpoints import tests, users, login_register, bookings, reports, addresses, pet
+from api.v1.endpoints import tests, users, login_register, bookings, reports, addresses, pet, geolocation
 
 api_router = APIRouter()
 api_router.include_router(tests.router, prefix="/tests", tags=["tests"])
@@ -9,3 +9,4 @@ api_router.include_router(bookings.router, prefix="/bookings", tags=["bookings"]
 api_router.include_router(reports.router, prefix="/reports", tags=["reports"])
 api_router.include_router(addresses.router, prefix="/addresses", tags=["addresses"])
 api_router.include_router(pet.router, prefix="/pets", tags=["pets"])
+api_router.include_router(geolocation.router, tags=["geolocation"])
